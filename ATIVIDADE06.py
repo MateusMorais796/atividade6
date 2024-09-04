b60e42bd6edf949680a5c7aa8ad41d1c08838ec7
MateusMorais796 <moraiscosta321@gmail.com>
2024-09-04 16:10:35 -0300

    Nome Completo

---
+++ ATIVIDADE06.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+p_nome = input('digite o primeiro nome ')
+s_nome = input('digite o sobrenome ')
+
+nome_completo = p_nome + s_nome
+
+print(f'o nome completo é {nome_completo}')
